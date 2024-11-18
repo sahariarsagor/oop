92c64c772fedcad41e1bab5f8239927d350f2209
Sahariar Sagor <contactsahariar@gmail.com>
2024-11-18 15:02:49 +0600

    problem 12

--- Loop Programming/problem12.java
+++ Loop Programming/problem12.java
@@ -2,23 +2,24 @@ import java.util.Scanner;
 public class problem12 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+            System.out.print("Please Enter a Number: ");
+            int n = sc.nextInt();
 
-        System.out.println("Enter a Number: ");
-        int n = sc.nextInt();
+            int lastDigit = n % 10; 
+            int firstDigit = n;
+            int count = 0; 
+            
+            while(firstDigit >= 10){
+                firstDigit = firstDigit / 10; 
+                count++; 
+            }
 
-        int lastDigit = n % 10; 
-        int firstDigit = n;
-        int count = 0;
+            int pow = (int) Math.pow(10, count);
+            int remaining = (n % pow) / 10; 
+            int finalResult = (lastDigit * pow) + (remaining * 10) + firstDigit;
 
-        while(firstDigit >= 10){
-            firstDigit = firstDigit / 10;
-            count++;
-        }
+            System.out.println("Final result is: " + finalResult);
 
-        int pro = (int) Math.pow(10, count);
-        int remaining = (n % pro)/10;
-        int finalResult = (lastDigit*1000) + (remaining*10) + firstDigit;
-        System.out.println("Final Result is: " + finalResult);
         sc.close();
     }
 }
