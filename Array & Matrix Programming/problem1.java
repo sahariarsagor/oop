d190b92107d45e96e47589e0b765ed90bc949b2f
Sahariar Sagor <contactsahariar@gmail.com>
2024-11-25 20:13:43 +0600

    array problem 01

--- Array & Matrix Programming/problem1.java
+++ Array & Matrix Programming/problem1.java
@@ -1,1 +1,27 @@
-hi 
+import java.util.Scanner;
+public class problem1{
+    public static void printArray(int[] arr, int index){
+        if(index == arr.length){
+            return;
+        }
+
+        System.out.println(arr[index]);
+        printArray(arr, index+1);
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the Size of array:");
+        int size = sc.nextInt();
+
+        int[] arr = new int[size];
+        System.out.println("Enter array Element: ");
+        for(int i = 0; i< size; i++){
+            System.out.print("Enter " + i + " no element: ");
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Printing Array Element: ");
+        printArray(arr, 0);
+        sc.close();
+    }
+}
