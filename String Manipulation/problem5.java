5ce370da421bc2765853ce82b7c98692235bb767
Sahariar Sagor <contactsahariar@gmail.com>
2024-12-01 20:46:06 +0600

    problem 05

---
+++ String Manipulation/problem5.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class problem5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a String in Lower case: ");
+        String userInput = sc.nextLine();
+        String result = userInput.toUpperCase();
+        System.out.println("Your String in Uppercase is: " + result);
+        sc.close();
+    }
+}
